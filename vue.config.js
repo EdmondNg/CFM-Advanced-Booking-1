fd098ecd269325211daf20705b2ad4aa078559e8
edmond <ncne@ymail.com>
2022-09-05 12:13:40 +0800

    edmond

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'https://keycloak.interlib.com.cn',
+        changeOrigin: true
+      },
+    }
+  },
+publicPath:'/app/'
+}
+
+
+
